5dc2bd560e632b52bbedab0645838f8af5543dfe
thirakhunrak <thirakhunrak@hotmail.com>
2022-03-21 14:56:46 +0700

    new algo and pyarrow

--- client.py
+++ client.py
@@ -6,39 +6,7 @@ from datetime import timezone
 import datetime
 import json
 from time import perf_counter
-
-# T = '2022-03-18T09:50:10.568317+00:00'
-# first
-# response = {'messages': [{'uuid': '0123456710', 'author': 'testname1', 'message': 'testmessage1', 'likes': 0},
-#                          {'uuid': '0123456711', 'author': 'testname2', 'message': 'testmessage2',
-#                           'likes': 1},
-#                          {'uuid': '0123456712', 'author': 'testname3', 'message': 'testmessage3',
-#                           'likes': 2},
-#                          {'uuid': '0123456713', 'author': 'testname4', 'message': 'testmessage4',
-#                           'likes': 3},
-#                          {'uuid': '0123456714', 'author': 'testname5', 'message': 'testmessage5', 'likes': 4}],
-#             'updates': []}
-
-# only update
-# response = {'messages': [],
-#             'updates': [{'uuid': '0123456712', 'author': '', 'message': 'asdf3', 'likes': 3, 'is_deleted': 0},
-#                         {'uuid': '0123456713', 'author': '', 'message': 'yqwef4', 'likes': 4, 'is_deleted': 0},
-#                         {'uuid': '0123456711', 'author': '', 'message': '', 'likes': -1, 'is_deleted': 0}]}
-
-# only delete
-# response = {'messages': [],
-#             'updates': [{'uuid': '0123456712', 'author': '', 'message': 'asdf3', 'likes': 3, 'is_deleted': 1},
-#                         {'uuid': '0123456713', 'author': '', 'message': 'yqwef4',
-#                          'likes': 4, 'is_deleted': 1},
-#                         {'uuid': '0123456711', 'author': '', 'message': '', 'likes': -1, 'is_deleted': 1}]}
-
-#create and update
-# response = {'messages': [{'uuid': '0123456715', 'author': 'testname6', 'message': 'testmessage6', 'likes': 0}],
-#             'updates': [{'uuid': '0123456714', 'author': '', 'message': 'krthdf5', 'likes': 10, 'is_deleted': 0}]}
-
-#create and delete
-# response = {'messages': [{'uuid': '0123456716', 'author': 'testname7', 'message': 'testmessage7', 'likes': 0}],
-#             'updates': [{'uuid': '0123456714', 'author': '', 'message': 'krthdf5', 'likes': 10, 'is_deleted': 1}]}
+import numpy as np
 
 
 def find(name):
@@ -47,47 +15,26 @@ def find(name):
             return os.path.join(root, name)
 
 
-def update_n_delete(df_saved, update):
-    if not update:
-        return df_saved
-
-    df_update = pd.DataFrame(update)
-    df_saved.set_index('uuid', inplace=True)
-    df_update.set_index('uuid', inplace=True)
-
-    for index, row in df_update.iterrows():
-        if row['is_deleted'] == 1:
-            if index in df_saved.index:
-                df_saved.drop(index, inplace=True)
-            continue
-        if len(str(row['message'])) > 0:
-            df_saved.loc[index, 'message'] = row['message']
-        if row['likes'] != -1:
-            df_saved.loc[index, 'likes'] = row['likes']
-
-    return df_saved
-
-
-def add_new(df_saved, create):
-    if not create:
-        return df_saved
-
-    df_create = pd.DataFrame(create)
-    df_create.set_index('uuid', inplace=True)
-
-    return pd.concat([df_saved, df_create])
+def update_value(df_merge, old_author, old_message, old_likes, new_author, new_message, new_likes):
+    df_merge.loc[pd.isnull(new_author),
+                 'author'] = old_author[pd.isnull(new_author)]
+    df_merge.loc[pd.isnull(new_message),
+                 'message'] = old_message[pd.isnull(new_message)]
+    df_merge.loc[pd.isnull(new_likes),
+                 'likes'] = old_likes[pd.isnull(new_likes)]
 
 
 def sync(endpoint):
+    t1_start = perf_counter()
     path = find('response.csv')
-    data_saved = pd.DataFrame()
+    data_saved = pd.DataFrame(columns=['uuid', 'author', 'message', 'likes'])
     timestamp = '0001-01-01T00:00:00'
 
     if path is not None:
         data_saved = pd.read_csv(path, dtype={'uuid': str},
                                  names=["uuid", "author", "message", "likes"])
-
-        data_saved.set_index('uuid', inplace=True)
+        print("reading file...")
+        # data_saved.set_index('uuid', inplace=True)
         timestamp = open("timestamp.txt", 'r').read()
 
     f = open('timestamp.txt', "w")
@@ -95,29 +42,67 @@ def sync(endpoint):
     f.close()
 
     i = 0
-    limit = 100000
+    limit = 70000
 
+    updates, messages, deletes = [], [], []
     while True:
         query = {'offset': i, 'limit': limit}
         i += limit
         json_response = requests.get(
             url=endpoint+timestamp, params=query)  # send param
+
         response = json_response.json()
-        if len(response['messages']) == 0 and len(response['updates'] == 0):
+        updates += response['updates']
+        messages += response['messages']
+        deletes += response['deletes']
+
+        tc_stop = perf_counter()
+        print("loop:", i, ":Elapsed time during the whole program in seconds:",
+              tc_stop-t1_start)
+
+        if len(response['messages']) == 0 and len(response['updates']) == 0:
             break
 
-        data_saved = add_new(data_saved, response['messages'])
-        data_saved = update_n_delete(data_saved, response['updates'])
+    t2_stop = perf_counter()
+    print("reciving:Elapsed time during the whole program in seconds:",
+          t2_stop-t1_start)
 
-    data_saved.to_csv('response.csv', header=False, index=True)
+    # add new message
+    data_saved = pd.concat(
+        [data_saved, pd.DataFrame(messages)])
 
+    # deleted message
+    data_saved.drop(
+        data_saved[data_saved['uuid'].isin(deletes)].index, inplace=True)
 
-def main(url):
-    t1_start = perf_counter()
-    sync(url)
+    # update message
+    df_update = pd.DataFrame(
+        updates, columns=['uuid', 'author', 'message', 'likes'])
+
+    df_merge = data_saved.merge(df_update[[
+                                'uuid', 'author', 'message', 'likes']], on='uuid', how='left', suffixes=('_', ''))
+    df_merge.replace('', np.nan, inplace=True)
+    df_merge.replace(-1.0, np.nan, inplace=True)
+
+    update_value(df_merge, df_merge['author_'].values, df_merge['message_'].values, df_merge['likes_'].values,
+                 df_merge['author'].values, df_merge['message'].values, df_merge['likes'].values)
+    df_merge.drop(columns=['author_', 'message_', 'likes_'], inplace=True)
+    df_merge['likes'] = df_merge['likes'].astype(int)
+    data_saved = df_merge
+
+    t3_stop = perf_counter()
+    print("merging:Elapsed time during the whole program in seconds:",
+          t3_stop-t1_start)
+
+    data_saved.to_csv('response.csv', header=False, index=False)
+
     t1_stop = perf_counter()
     print("Elapsed time during the whole program in seconds:",
           t1_stop-t1_start)
+
+
+def main(url):
+    sync(url)
 
 
 main(sys.argv[1])
